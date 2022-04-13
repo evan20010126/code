452fc0dbf898d76187393cb0197f14e93fb4d70b
evan20010126 <55246608+evan20010126@users.noreply.github.com>
2022-04-13 12:56:09 +0800

    vision

--- python/Machine_vision/my_test.py
+++ python/Machine_vision/my_test.py
@@ -1,65 +1,11 @@
-try:
-    import pafy
-except:
-    # 改天pafy修正完畢後，下面指令可以改為!pip install pafy
-    # !pip install git+https://github.com/Cupcakus/pafy
-    # !pip install youtube_dl
-    import pafy
+# 示範怎麼把高斯的kernal擺上去
+import tensorflow as tf
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
-try:
-    import cv2
-except:
-    # !pip install opencv-python
-    import cv2
 
-url = "https://www.youtube.com/watch?v=X0uaGjVUY_E&t=611s"
-ty_video = pafy.new(url, basic=False, gdata=False)
-best = ty_video.getbest(preftype="mp4")
-video = cv2.VideoCapture(best.url)
-
-if video is not None:
-    fps = int(video.get(cv2.CAP_PROP_FPS))
-    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    print('Frame rate:', fps, 'Frame width:', width, 'Frame height:', height)
-
-    frame_num = 0
-    play_flag = 1
-    total_frame = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-
-    def set_frame_number(x):
-        global frame_num, play_flag
-        if play_flag == 0:
-            frame_num = x
-            video.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
-        return
-
-    def play(x):
-        global play_flag
-        play_flag = x
-        return
-
-    cv2.namedWindow('youtube')
-    cv2.createTrackbar('frame no.', 'youtube', 0,
-                       total_frame-1, set_frame_number)
-    cv2.createTrackbar('play', 'youtube', 0, 1, play)
-    cv2.setTrackbarPos('play', 'youtube', play_flag)
-
-    while True:
-
-        cv2.setTrackbarPos('frame no.', 'youtube', frame_num)
-
-        if play_flag:
-            grabbed, frame = video.read()
-            cv2.imshow('youtube', frame)
-            frame_num += 1
-
-        k = cv2.waitKey(1000//fps)
-        if k == 27:
-            break
-
-    cv2.destroyAllWindows()
-    video.release()
-
-else:
-    print('cannot open {}'.format(url))
+# 9x9的矩陣來放 吐一塊9x9的高斯kernal 呼叫opencv幫我填高斯的kernal 目的: 三個通道每個通道分別做高斯模糊
+gkernel = cv2.getGaussianKernel(9, 9.)
+gfilter2d = np.dot(gkernel, gkernel.T)
+print(gkernel)
