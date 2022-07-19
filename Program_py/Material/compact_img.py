7349b0c42140292468fd0803596e63a5780ffb6c
evan20010126 <55246608+evan20010126@users.noreply.github.com>
2022-07-19 23:21:24 +0800

    summary code

--- Program_py/Material/compact_img.py
+++ Program_py/Material/compact_img.py
@@ -1,10 +1,10 @@
 from PIL import ImageGrab
 from PIL import Image
 
-file_name = "store_hat.jpg"
+file_name = "transformer_model_01.png"
 
 img = Image.open(file_name)
 
-img = img.resize((613, 535), Image.BILINEAR)
+img = img.resize((678, 2753), Image.BILINEAR)
 
 img.save("modify"+file_name)
